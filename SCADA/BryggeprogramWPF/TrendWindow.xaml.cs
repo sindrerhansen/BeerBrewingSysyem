7239d6534bbbbd5646e708faf76ff7d47f51085c
sindrerhansen <sindrerhansen@gmail.com>
2015-03-16 08:13:36 +0100

    Adding trend view

---
+++ SCADA/BryggeprogramWPF/TrendWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Axes;
+
+namespace BryggeprogramWPF
+{
+    /// <summary>
+    /// Interaction logic for TrendWindow.xaml
+    /// </summary>
+    public partial class TrendWindow : Window
+    {
+        public TrendWindow()
+        {
+            
+            InitializeComponent();
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+            //Plot.InvalidatePlot();
+        }
+    }
+}
