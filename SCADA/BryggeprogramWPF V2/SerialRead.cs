94c543f15f3f8b27ef63f884998922efe2a45fab
sindrerhansen <sindrerhansen@gmail.com>
2015-06-29 11:32:44 +0200

    Tuning

---
+++ SCADA/BryggeprogramWPF V2/SerialRead.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BryggeprogramWPF_V2
+{
+    class SerialRead
+    {
+    }
+}
