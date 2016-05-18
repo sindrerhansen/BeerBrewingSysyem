500122b960f96e62a58744b668754f54ef633596
Sindre Rynning Hansen <sindrerhansen@gmail.com>
2016-05-18 22:58:37 +0200

    Adding i2c com

--- Arduino/FlowSensor/FlowSensor.ino
+++ Arduino/FlowSensor/FlowSensor.ino
@@ -1,11 +1,19 @@
 
+#include <Wire.h>
+
 volatile unsigned long  total_flow;
 float totalLiter;
 unsigned int  l_hour;          // Calculated litres/hour                      
 static int flowmeter = 2;  // Flow Meter Pin number
 unsigned long cloopTime;
 String input_String;
+static String MyName = "FM";
 bool input_StringComplete;
+
+
+#define MEGA_MASTER_ADDRESS 0x9
+#define NANO_FM_ADDRESS 0x8
+
 
 void flow()                  // Interruot function
 {
@@ -17,6 +25,8 @@ void setup()
 {
 	pinMode(flowmeter, INPUT);
 	Serial.begin(38400);
+	Wire.begin(NANO_FM_ADDRESS);
+	Wire.onReceive(wireReceiveEvent);
 	total_flow = 0;
 	attachInterrupt(0, flow, RISING); // Setup Interrupt 
 	sei();                            // Enable interrupts  
@@ -32,13 +42,34 @@ void serialEvent(){
 		}
 	}
 }
+
+void wireReceiveEvent(int howMany) {
+	char charReadBuffer[20] = "";
+	int charIndicator = 0;
+	while (Wire.available() > 0) {
+		char c = Wire.read();
+		charReadBuffer[charIndicator] = c;
+		charIndicator++;
+	}
+	String inStr(charReadBuffer);
+	input_String = inStr;
+	input_StringComplete = true;
+
+}
 void loop()
 {
 	if (millis() >= (cloopTime + 500))
 	{
 		cloopTime = millis();			     // Updates cloopTime
         totalLiter = total_flow / 444.4444444 ;
-        Serial.println(totalLiter);						
+        Serial.println(totalLiter);		
+		// Sending on I2C buss
+		char charSendBuffer[20];
+		String sendString = MyName + String(totalLiter);
+		sendString.toCharArray(charSendBuffer, 20);
+		Wire.beginTransmission(MEGA_MASTER_ADDRESS);
+		Wire.write(charSendBuffer);
+		Wire.endTransmission();
 
 	}
 
